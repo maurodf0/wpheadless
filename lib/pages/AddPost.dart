9a076d737b0287bac8df9f9296f54cc4a84bbf32
Mauro <maurodefalco96@gmail.com>
2026-04-28 01:24:18 +0200

    Aggiungi il modulo per la creazione di nuovi post con gestione del titolo e contenuto

--- lib/pages/AddPost.dart
+++ lib/pages/AddPost.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 class Addpost extends StatefulWidget {
@@ -8,14 +9,62 @@ class Addpost extends StatefulWidget {
 }
 
 class _AddpostState extends State<Addpost> {
+
+  final titleController = TextEditingController();
+  final contentController = TextEditingController();
+
+  final dio = Dio();
+
+Future<void> addArticle() async {
+  try {
+    await dio.post(
+      'https://jsonplaceholder.typicode.com/posts',
+      data: {
+        'title': titleController.text,
+        'body': contentController.text,
+        'userId': 1,
+      },
+    );
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(content: Text('Post inviato')),
+    );
+  } catch (e) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(content: Text('Errore')),
+    );
+  }
+}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Add Post'),
       ),
-      body: Center(
-        child: Text('Form per aggiungere un nuovo post'),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: titleController,
+              decoration: InputDecoration(
+                labelText: 'Title',
+              ),
+            ),
+            SizedBox(height: 20.0,),
+            TextField(
+              controller: contentController,
+              decoration: InputDecoration(
+                labelText: 'Content',
+              ),
+            ),
+            SizedBox(height: 20.0,),
+            ElevatedButton(
+              onPressed: addArticle,
+            child: Text('Add Post to Blog'))
+          ],
+        ),
       ),
     );
   }
